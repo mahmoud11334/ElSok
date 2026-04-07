3cca1734372f280e5dd34560d2b46453b545b540
mahmoud11334 <7oka12345678910@gmail.com>
2026-04-07 02:36:36 +0200

    ver3

---
+++ run.bat
@@ -0,0 +1,5 @@
+E:
+cd E:\Data\VS code\test\alsok
+git add .
+git commit -m "ver3"
+git push -u origin main
--- run2.bat
+++
@@ -1,8 +0,0 @@
-E:
-cd E:\Data\VS code\test\alsok
-git init
-git add .
-git commit -m "ver3"
-git branch -M main
-git remote add origin https://github.com/mahmoud11334/ElSok.git
-git push -u origin main
