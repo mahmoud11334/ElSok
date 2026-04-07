e37dd02dade9e02caaee61dd17e2408effc07f37
mahmoud11334 <7oka12345678910@gmail.com>
2026-04-07 02:31:36 +0200

    Version3

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ E:
 cd E:\Data\VS code\test\alsok
 git init
 git add .
-git commit -m "Version 3"
+git commit -m "Version3"
 git branch -M main
 git remote add origin https://github.com/mahmoud11334/ElSok.git
 git push -u origin main
